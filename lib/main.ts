df82a050a7053845de13081e25295a19c25f924e
Mgen <m@mgenware.com>
2017-10-16 00:34:56 +0800

    Use unhandledRejection to print more info on async errors

--- lib/main.ts
+++ lib/main.ts
@@ -3,3 +3,9 @@ export * from './eventArgs';
 export { default as ContentGenerator } from './contentGenerator';
 export * from 'barbary';
 export { default as Config } from './config';
+
+/* tslint:disable:no-console */
+process.on('unhandledRejection', (err) => {
+  console.log('Caught unhandledRejection');
+  console.log(err);
+});
