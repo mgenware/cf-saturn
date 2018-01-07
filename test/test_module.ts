41c0a21e0380a1449d4fdb72ad6ca6acabd06e28
Mgen <m@mgenware.com>
2018-01-07 12:59:33 +0800

    Switch to jest

--- test/test_module.ts
+++
@@ -1,8 +0,0 @@
-const main = require('../..');
-import * as assert from 'assert';
-
-describe('require this module', () => {
-  it('No exception is thrown', () => {
-    assert.equal(typeof main.start, 'function');
-  });
-});
