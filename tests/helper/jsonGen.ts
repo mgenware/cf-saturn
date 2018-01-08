7f62c1af856005f0f8f933dc25ec64eb95401157
Mgen <m@mgenware.com>
2018-01-08 18:37:25 +0800

    Update relative path in jsonGen.ts

--- tests/helper/jsonGen.ts
+++ tests/helper/jsonGen.ts
@@ -1,9 +1,10 @@
 
 import objectFromDirectory from 'fx45-node';
+import * as nodepath from 'path';
 
 if (process.argv.length >= 3) {
   const name = process.argv[2];
-  const results = objectFromDirectory('./saturn/dist/' + name);
+  const results = objectFromDirectory(nodepath.resolve(__dirname, '../../dist/tests/saturn/dist/' + name));
   // tslint:disable-next-line no-console
   console.log(results);
 } else {
