c2a2bb81a12531c9f873d1147a422246d368ad08
Mgen <m@mgenware.com>
2018-01-12 23:48:04 +0800

    Update pathBar file name

--- lib/defs.ts
+++ lib/defs.ts
@@ -7,9 +7,8 @@ export default {
     contentExt: '.g.html',
     titleExt: '.t.g.txt',
     seoTitleExt: '.t_seo.g.txt',
-    pathBarFile: '__dir.path.g.html',
     dirTitleFile: '__dir.t.g.txt',
     dirSeoTitleFile: '__dir.t_seo.g.txt',
-    dirPathBarFile: '__dir.path_bar.g.txt',
+    dirPathBarFile: '__dir.path.g.html',
   },
 };
