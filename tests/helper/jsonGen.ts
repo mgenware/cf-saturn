f203d20b21b2a618b49d28b3095f6b721f0d4209
Mgen <m@mgenware.com>
2018-01-14 10:43:38 +0800

    Fix deeply nested object not showing

--- tests/helper/jsonGen.ts
+++ tests/helper/jsonGen.ts
@@ -1,12 +1,13 @@
 
 import objectFromDirectory from 'fx45-node';
 import * as nodepath from 'path';
+import * as util from 'util';
 
 if (process.argv.length >= 3) {
   const name = process.argv[2];
   const results = objectFromDirectory(nodepath.resolve(__dirname, '../../dist/tests/saturn/dist/' + name));
   // tslint:disable-next-line no-console
-  console.log(results);
+  console.log(util.inspect(results, false, null, true));
 } else {
   process.exit(1);
 }
