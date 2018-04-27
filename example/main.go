85e40446e375b34d3ab6a36004822acae05d6f84
Mgen <m@mgenware.com>
2018-04-27 17:33:08 +0800

    Update example

--- example/main.go
+++ example/main.go
@@ -4,17 +4,41 @@ import (
 	"cf-saturn"
 	"fmt"
 	"log"
+	"net/http"
+	"os"
 )
 
+var builder *saturn.Builder
+
+func init() {
+	b, err := saturn.NewBuilder("./data")
+	if err != nil {
+		log.Fatal(err)
+	}
+	b.PrefixURL = "/my-lib"
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	builder = b
+}
+
 func main() {
-	builder, err := saturn.NewBuilder("./data")
-	builder.PrefixURL = "/my-lib"
-	if err != nil {
-		log.Fatal(err)
-	}
-	page, err := builder.Build("node.js/fs/readFileSync.md")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Print(page)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		page, err := builder.Build(r.URL.Path)
+		if err != nil {
+			if os.IsNotExist(err) {
+				fmt.Fprint(w, "File not found")
+				return
+			}
+			log.Fatal(err)
+		}
+
+		w.Header().Set("Content-Type", "text/html")
+		content := fmt.Sprintf("<html><body><b>%v</b><br/>%v</body></html>", r.URL.Path, page)
+		fmt.Fprint(w, content)
+	})
+
+	log.Print(builder)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
